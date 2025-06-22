ea733545ca64bac3cc5162abccf6deb1c7858f79
Angel Daniel Angeles Toribio <daniel_1234@hotmail.es>
2025-06-22 10:42:19 -0400

    Update patterns.py

--- patterns.py
+++ patterns.py
@@ -1,1 +1,8 @@
-
+def detect_engulfing(df):
+    df['bullish_engulfing'] = (
+        (df['close'].shift(1) > df['open'].shift(1)) &  # vela anterior bajista
+        (df['open'] < df['close']) &                    # vela actual alcista
+        (df['open'] < df['close'].shift(1)) &           # apertura por debajo del cierre anterior
+        (df['close'] > df['open'].shift(1))             # cierre por encima de apertura anterior
+    )
+    return df
